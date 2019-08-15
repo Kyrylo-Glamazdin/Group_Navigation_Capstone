623c3ff24f900263b942352c3bfe8d3023f9b974
zhouyanpu <zhouyanpu@gmail.com>
2019-08-15 14:50:03 -0400

    new

--- ui/src/App.js
+++ ui/src/App.js
@@ -1,46 +1,44 @@
-
-import React, {Component} from 'react';
-import './App.css';
-import Dashboard from './Components/Dashboard.js';
-import Map from './Components/Map.js'
-import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
-import {connect} from 'react-redux';
+import React, { Component } from "react";
+import "./App.css";
+import Dashboard from "./Components/Dashboard.js";
+import Map from "./Components/Map.js";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { connect } from "react-redux";
 import Login from "./Components/login";
-import Form from './Components/Form'
+import Form from "./Components/Form";
 
-class App extends Component{
-  render(){
-
-    const LoginComponent = () => (<Login />);
+class App extends Component {
+  render() {
+    const LoginComponent = () => <Login />;
 
     const MainPageComponent = () => (
       <div className="App">
         <Dashboard />
-        <div id="content">
-          {<Map />}
-        </div>
+        <div id="content">{<Map />}</div>
       </div>
     );
 
-    const FormComponent = () => (<Form />);
+    const FormComponent = () => <Form />;
 
     return (
-        <Router>
-          <Switch>
-            <Route exact path= "/" component={LoginComponent}/> 
-            <Route exact path= "/dashboard" component={MainPageComponent}/>
-            <Route exact path= "/dashboard/form" component={FormComponent}/>
-          </Switch>
-        </Router>
+      <Router>
+        <Switch>
+          <Route exact path="/" component={LoginComponent} />
+          <Route exact path="/dashboard" component={MainPageComponent} />
+          <Route exact path="/dashboard/form" component={FormComponent} />
+        </Switch>
+      </Router>
     );
   }
 }
-
 
 const mapStateToProps = state => {
   return {
     users: state.users
   };
-}
+};
 
-export default connect(mapStateToProps,{})(App);
+export default connect(
+  mapStateToProps,
+  {}
+)(App);
