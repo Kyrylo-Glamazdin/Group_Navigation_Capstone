eb5d646ce01af46ef858492efed20e8d5ec3a293
zhouyanpu <zhouyanpu@gmail.com>
2019-08-17 14:51:52 -0400

    gogogo

--- ui/src/Components/Dashboard.js
+++ ui/src/Components/Dashboard.js
@@ -1,66 +1,87 @@
-import React, {Component} from 'react';
-import './Dashboard.css';
-import GroupGrid from './GroupGrid';
-import {Redirect} from 'react-router';
-import {connect} from 'react-redux';
-import {addUsers} from '../Actions/index';
+import React, { Component } from "react";
+import decode from "jwt-decode";
+import "./Dashboard.css";
+import GroupGrid from "./GroupGrid";
+import { Redirect } from "react-router";
+import { connect } from "react-redux";
+import { addUsers } from "../Actions/index";
 
-class Dashboard extends Component{
-    constructor(props){
-        super(props);
-        this.state = {
-            redirect: false
-        }
-        this.openNav = this.openNav.bind(this);
-        this.closeNav = this.closeNav.bind(this);
-    }
-    openNav() {
-        document.getElementById("dashboard").style.width = "25vw";
-        document.getElementById("content").style.marginLeft = "25vw";
-    }
+class Dashboard extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      redirect: false
+    };
+    this.openNav = this.openNav.bind(this);
+    this.closeNav = this.closeNav.bind(this);
+  }
 
-    closeNav() {
-        document.getElementById("dashboard").style.width = "1vw";
-        document.getElementById("content").style.marginLeft = "1vw";
-    }
+  componentDidMount = async () => {
+    try {
+      if (!localStorage.token) console.log("!!!!!!!");
 
-    sendRequest = async () => {
-        // await axios.get('localhost:5000/users')
-        // .then(res => {
-        //     //send response to action creator
-        //      this.props.addUsers(res);
-        // })
-        // .catch(e => {
-        //     console.log(e);
-        // })
-        await this.setState({redirect: true});  
-    }
+      // const user = decode(localStorage.token);
 
-    render(){
-        if(this.state.redirect)
-        {
-            return(
-                <Redirect to="/dashboard/form"/>
-            )
-        }
-        else
-        {
-            return(
-                <div id = "dashboard">
-                    <div className="title" onMouseOut={this.closeNav} onMouseOver={this.openNav}> Dashboard </div>
-                    <div> Profile Name</div>
-                    <GroupGrid></GroupGrid>
-                    <button onClick={this.sendRequest}> Create New Group </button>
-                </div>
-            )
-        }
+      // await this.setState({ user });
+    } catch (error) {
+      // console.log( this.props);
+      // window.location = "/";
     }
+  };
+
+  openNav() {
+    document.getElementById("dashboard").style.width = "25vw";
+    document.getElementById("content").style.marginLeft = "25vw";
+  }
+
+  closeNav() {
+    document.getElementById("dashboard").style.width = "1vw";
+    document.getElementById("content").style.marginLeft = "1vw";
+  }
+
+  sendRequest = async () => {
+    // await axios.get('localhost:5000/users')
+    // .then(res => {
+    //     //send response to action creator
+    //      this.props.addUsers(res);
+    // })
+    // .catch(e => {
+    //     console.log(e);
+    // })
+    await this.setState({ redirect: true });
+  };
+
+  render() {
+    if (this.state.redirect) {
+      return <Redirect to="/dashboard/form" />;
+    } else if (!localStorage.token) {
+      return <Redirect to="/" />;
+    } else {
+      return (
+        <div id="dashboard">
+          <div
+            className="title"
+            onMouseOut={this.closeNav}
+            onMouseOver={this.openNav}
+          >
+            Dashboard
+          </div>
+          <div> Profile Name</div>
+          <GroupGrid />
+          <button onClick={this.sendRequest}> Create New Group </button>
+        </div>
+      );
+    }
+  }
 }
 
 const mapStateToProps = state => {
-    return {
-        groups: state.groups
-    };
-}
+  return {
+    groups: state.groups
+  };
+};
 
-export default connect(mapStateToProps,{addUsers})(Dashboard)
+export default connect(
+  mapStateToProps,
+  { addUsers }
+)(Dashboard);
